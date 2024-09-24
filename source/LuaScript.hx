19722b52dbc9bd52ce206cc540fa016feeffe8c1
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-09-24 09:30:19 +0700

    wip lua added (um, will do if the `hl` target get the last version)

---
+++ source/LuaScript.hx
@@ -0,0 +1,17 @@
+#if cpp
+package;
+
+import flixel.FlxBasic;
+import vm.lua.Lua;
+
+// im don't sure is working tho?
+class LuaScript extends FlxBasic
+{
+	public static var lua:Lua;
+
+	public function new(file:String)
+	{
+		super();
+	}
+}
+#end
