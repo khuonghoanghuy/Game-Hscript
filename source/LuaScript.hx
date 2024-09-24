dea2328c2ccf80535e5812d1f3335818e9a34e7f
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-09-24 09:33:33 +0700

    demo working

--- source/LuaScript.hx
+++ source/LuaScript.hx
@@ -2,6 +2,7 @@
 package;
 
 import flixel.FlxBasic;
+import sys.io.File;
 import vm.lua.Lua;
 
 // im don't sure is working tho?
@@ -9,9 +10,26 @@ class LuaScript extends FlxBasic
 {
 	public static var lua:Lua;
 
-	public function new(file:String)
+	public function new(file:String, ?execute:Bool = true)
 	{
 		super();
+		lua.setGlobalVar("game", PlayState.instance);
+
+		if (execute)
+			this.executeFile(file);
+		lua.destroy();
+	}
+
+	function executeFile(file)
+	{
+		try
+		{
+			lua.run(File.getContent(file));
+		}
+		catch (e)
+		{
+			trace(e.message);
+		}
 	}
 }
 #end
